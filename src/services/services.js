3029c6988e8afc8fe52a2412a68ddfe05e825ad1
ZvezdaNeycheva <138030249+ZvezdaNeycheva@users.noreply.github.com>
2024-04-11 14:31:18 +0300

    Update services.js

--- src/services/services.js
+++ src/services/services.js
@@ -1,45 +1,5 @@
-import { db } from "./firebase";
-import { ref, push, get, query, equalTo, orderByChild, update } from 'firebase/database';
 
-const refPic = db.ref('pictures');
-
-
-// export const uploadPic =async(file)=>{
-//     refPic.push().set({
-//         id: '',
-//         picUrl: '',
-//         title: ,
-//         description: ,
-//       });
-// }
-
-export const getAllPics = async (search) => {
-    const snapshot = await get(query(ref(db, 'pictures'), orderByChild('createdOn')));
-    if (!snapshot.exists()) {
-      return [];
-    }
-  
-    const pic = Object.keys(snapshot.val()).map(key => ({
-      id: key,
-      ...snapshot.val()[key],
-      createdOn: new Date(snapshot.val()[key].createdOn).toString(),
-      likedBy: snapshot.val()[key].likedBy ? Object.keys(snapshot.val()[key].likedBy) : [],
-    }))
-      .filter(t => t.title.toLowerCase().includes(search.toLowerCase()));
-  
-    return pic;
-  };
-export const findPicById =async(id)=>{
-
-}
-export const editPicById =async(id)=>{
-
-}
-export const deletePicById =async(id)=>{
-
-}
-
-export const getAllpics = async(search) => {
+export const getAllPics = async(search) => {
       const result = await fetch(`http://localhost:3000/pics?search=${search}`)
       
       if (!result.ok) {
@@ -49,7 +9,7 @@ export const getAllpics = async(search) => {
       return result.json();
     };
     
-    export const getpicById = async(id) => {
+    export const getPicById = async(id) => {
       const result = await fetch(`http://localhost:3000/pics/${id}`)
       
       if (!result.ok) {
@@ -59,7 +19,7 @@ export const getAllpics = async(search) => {
       return result.json();
     };
     
-    export const addpic = async(pic) => {
+    export const uploadPic = async(pic) => {
       const result = await fetch("http://localhost:3000/pics", {
         method: "POST",
         headers: {
@@ -73,7 +33,7 @@ export const getAllpics = async(search) => {
       }
     };
     
-    export const updatepic = async(pic) => {
+    export const editPicById = async(pic) => {
       const result = await fetch(`http://localhost:3000/pics/${pic.id}`, {
         method: "PUT",
         headers: {
@@ -86,3 +46,13 @@ export const getAllpics = async(search) => {
         return console.log('Something went wrong');
       }
     };
+    export const deletePicById = async(pic) => {
+        const result = await fetch(`http://localhost:3000/pics/${pic.id}`, {
+          method: "DELETE",
+          body: JSON.stringify(pic)
+        });
+      
+        if (!result.ok) {
+          return console.log('Something went wrong');
+        }
+      };

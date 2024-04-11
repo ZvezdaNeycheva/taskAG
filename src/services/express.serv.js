b9f0574b8caad883519b51f66adcd641d599f31f
ZvezdaNeycheva <138030249+ZvezdaNeycheva@users.noreply.github.com>
2024-04-11 15:11:17 +0300

    Update express.serv.js

--- src/services/express.serv.js
+++ src/services/express.serv.js
@@ -1,33 +1,77 @@
-const express = require("express");
+import express from 'express';
 const app = express();
+app.use(express.json());
 const port = 3000;
-const router =express.Router()
-import {getAllPics}from './controllers.js'
 
-router.route('/').get(getAllPics)
+let uuid= ()=>{ self.crypto.randomUUID()};
 
-app.post("/", (req, res) => {
-    res.status(200).json({
-      id:'',
-      picUrl:''
-    });
+const pics = [{
+    id: uuid,
+    picUrl: '',
+    title: 'title',
+    description: 'description',
+  }];
+
+  
+app.get('/pics', (req, res) => {
+    if (req.query.search) {
+      res.json(pics.filter((pic) => pic.id.includes(req.query.search)));
+    }
+  
+    res.json(pics);
   });
-
-  app.put("/:id", (req, res) => {
-    res.status(200).json({
-      id:'',
-      picUrl:''
-    });
+  
+  app.get('/pics/:id', (req, res) => {
+    const pic = pics.find((pic) => pic.id === req.params.id);
+    if (!pic) {
+      res.sendStatus(404);
+      return;
+    }
+  
+    setTimeout(() => {
+    res.json(pic);
+    }, 1000);
   });
-
-  app.delete("/:id", (req, res) => {
-    res.status(200).json({
-      id:'',
-      picUrl:''
-    });
+  
+  app.post('/pics', (req, res) => {
+    const pic = {
+      id: req.body.uuid,
+      picUrl: req.body.picUrl,
+      title: req.body.title,
+      description: req.body.description,
+    };
+    pics.push(pic);
+  
+    res.json(pic);
   });
-app.listen(port, () => {
-  console.log(`listening on port ${port}!`);
-});
+  
+  app.put('/pics/:id', (req, res) => {
+    const pic = pics.find((pic) => pic.id === req.params.id);
+  
+    if (!pic) {
+      res.sendStatus(404);
+      return;
+    }
+    
+    pic.picUrl= req.body.picUrl,
+    pic.title= req.body.title
+    pic.description= req.body.description,
 
-module.exports=express.Router
+    res.json(pic);
+  });
+  
+  app.delete('/pics/:id', (req, res) => {
+    const index = pics.findIndex((pic) => pic.id === req.params.id);
+  
+    if (index === -1) {
+      res.sendStatus(404);
+      return;
+    }
+    pics.splice(index, 1);
+  
+    res.sendStatus(204);
+  });
+  
+  app.listen(3000, () => {
+    console.log(`Server listening on port ${port}`);
+  });
